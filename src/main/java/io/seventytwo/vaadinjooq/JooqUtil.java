41d0abefbe514e2a39209f8f0c25cb8287a6c3c8
Simon Martinelli <simon@72.services>
2019-05-09 08:38:43 +0200

    removed Guava dependency

--- src/main/java/io/seventytwo/vaadinjooq/JooqUtil.java
+++ src/main/java/io/seventytwo/vaadinjooq/JooqUtil.java
@@ -1,6 +1,5 @@
 package io.seventytwo.vaadinjooq;
 
-import com.google.common.base.CaseFormat;
 import com.vaadin.flow.data.provider.QuerySortOrder;
 import com.vaadin.flow.data.provider.SortDirection;
 import org.jooq.Field;
@@ -15,28 +14,50 @@ public class JooqUtil {
     private JooqUtil() {
     }
 
-    public static OrderField[] createOrderBy(Table table, List<QuerySortOrder> sortOrders) {
-        List<OrderField<?>> list = sortOrders.stream().map(sortOrder -> {
-            Field<?> field = table.field(getFieldName(sortOrder.getSorted()));
-            if (sortOrder.getDirection() == SortDirection.ASCENDING) {
-                return field.asc();
-            } else {
-                return field.desc();
-            }
-        }).collect(Collectors.toList());
-        return list.toArray(new OrderField[list.size()]);
-    }
-
     public static String getPropertyName(Field<?> field) {
-        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, field.getName());
-    }
-
-    private static String getFieldName(String propertyName) {
-        return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, propertyName);
+        String fieldName = field.getName();
+        char[] chars = fieldName.toLowerCase().toCharArray();
+        boolean wasUnderScore = false;
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            if (c == '_') {
+                wasUnderScore = true;
+            } else {
+                if (wasUnderScore) {
+                    sb.append(Character.valueOf(c).toString().toUpperCase());
+                } else {
+                    sb.append(c);
+                }
+                wasUnderScore = false;
+            }
+        }
+        return sb.toString();
     }
 
     public static Field<?> getField(Table table, String propertyName) {
         String fieldName = getFieldName(propertyName);
         return (Field<?>) table.field(fieldName);
     }
+
+    public static OrderField[] createOrderBy(Table table, List<QuerySortOrder> sortOrders) {
+        List<OrderField<?>> list = sortOrders.stream().map(sortOrder -> {
+            Field<?> field = table.field(getFieldName(sortOrder.getSorted()));
+            return sortOrder.getDirection() == SortDirection.ASCENDING ? field.asc() : field.desc();
+        }).collect(Collectors.toList());
+
+        return list.toArray(new OrderField[list.size()]);
+    }
+
+    private static String getFieldName(String propertyName) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : propertyName.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append("_" + c);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString().toUpperCase();
+    }
+
 }
